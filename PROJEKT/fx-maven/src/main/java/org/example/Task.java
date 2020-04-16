52a5636a03081c8ae82c9569b32c25656d66e59b
Animus Vox <Nishikino666@gmail.com>
2020-04-16 20:01:55 +0200

    Dodanie TableView Mały fix do Klasy Task

--- PROJEKT/fx-maven/src/main/java/org/example/Task.java
+++ PROJEKT/fx-maven/src/main/java/org/example/Task.java
@@ -2,34 +2,47 @@ package org.example;
 
 public class Task {
 
-    //ID detalu
-    private int id;
-    //Nazwa detalu/pracy
-    private String name;
-    //Index detalu np: 12.INVO.1120/15
-    private String index;
-    //Ile do wykonania
-    private int quantity;
-    //Ile wykonano
-    private int done;
-    //Status
-    private String status;
+        //ID detalu
+        private int id;
+        //Nazwa detalu/pracy
+        private String name;
+        //Index detalu np: 12.INVO.1120/15
+        private String index;
+        //Ile do wykonania
+        private int quantity;
+        //Ile wykonano
+        private int done;
+        //Status
+        private String status;
+        //piorytet
+        private String priority;
 
-    public Task(String name, String index, int quantity, int done, String status) {
+    public Task(String name, String index, int quantity, int done, String status, String priority) {
         this.name = name;
         this.index = index;
         this.quantity = quantity;
         this.done = done;
         this.status = status;
+        this.priority = priority;
     }
 
-    public Task(int id, String name, String index, int quantity, int done, String status) {
+    public Task(int id, String name, String index, int quantity, int done, String status, String priority) {
         this.id = id;
         this.name = name;
         this.index = index;
         this.quantity = quantity;
         this.done = done;
         this.status = status;
+        this.priority = priority;
+    }
+
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
     }
 
     public int getId() {
--- PROJEKT/fx-maven/src/main/java/org/example/TasksController.java
+++ PROJEKT/fx-maven/src/main/java/org/example/TasksController.java
@@ -1,27 +1,41 @@
 package org.example;
 
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ListView;
-
-import java.util.ArrayList;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class TasksController {
     public ChoiceBox actionBox;
-    public ListView tasksList;
-
+    public TableView<Task> tasksList;
+    public TableColumn<Object, Object> id;
+    public TableColumn<Object, Object> name;
+    public TableColumn<Object, Object> index;
+    public TableColumn<Object, Object> quantity;
+    public TableColumn<Object, Object> count;
+    public TableColumn<Object, Object> status;
+    public TableColumn<Object, Object> priority;
 
 
     public void initialize() {
-        tasksList.setEditable(true);
-        Task task = new Task(1,"Detal","12.INVO.1120/15",20,5,"W realizacji");
+        id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        index.setCellValueFactory(new PropertyValueFactory<>("index"));
+        quantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        count.setCellValueFactory(new PropertyValueFactory<>("done"));
+        status.setCellValueFactory(new PropertyValueFactory<>("status"));
+        priority.setCellValueFactory(new PropertyValueFactory<>("priority"));
+
+        Task task = new Task(1,"Wyrób Metalowy","11.INVO.1120/15",3,1,"W realizacji","Wysoki");
+        Task task2 = new Task(2,"Wyrób Drewniany","12.INVO.1195/16",20,20,"Gotowy","Sredni");
+        Task task3 = new Task(3,"Wyrób Kompozytowy","13.INVO.1132/18",100,45,"Przerwany","Niski");
+
         actionBox.getItems().removeAll(actionBox.getItems());
         actionBox.getItems().addAll("Zmień Stan", "Raportuj", "Gen. Raport", "Wstrzymaj");
         actionBox.getSelectionModel().select("Zmień Stan");
-        ArrayList <String> testlist = new ArrayList<>();
-        testlist.add("test1");
-        testlist.add("test2");
-        testlist.add("test3");
-        testlist.add(task.toString());
-        tasksList.getItems().addAll(testlist);
+
+
+        tasksList.getItems().add(task);
+        tasksList.getItems().add(task2);
+        tasksList.getItems().add(task3);
+
     }
 }
